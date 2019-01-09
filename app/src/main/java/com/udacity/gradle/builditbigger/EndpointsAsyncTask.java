d757473727d0ce7802cc82cb7a2427daf9724516
Zizoh Anto <zizohanto@hotmail.com>
2019-01-09 23:54:38 +0100

    feat: first commit

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,71 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.jokeApi.JokeApi;
+
+import java.io.IOException;
+
+class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private static final String TAG = EndpointsAsyncTask.class.getSimpleName();
+    private static JokeApi jokeApiService = null;
+    private OnJokeEventListener mOnJokeEventListener;
+
+    EndpointsAsyncTask(OnJokeEventListener listener) {
+        mOnJokeEventListener = listener;
+    }
+
+    public static void getNewInstance(OnJokeEventListener listener) {
+        new EndpointsAsyncTask(listener).execute();
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mOnJokeEventListener.onGetJokeStarted();
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        if (jokeApiService == null) {  // Only do this once
+            JokeApi.Builder builder = new JokeApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setApplicationName("Build It Bigger")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+            jokeApiService = builder.build();
+        }
+        try {
+            return jokeApiService.getJokeService().execute().getData();
+        } catch (IOException e) {
+            Log.d(TAG, "doInBackground: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        mOnJokeEventListener.onJokeReceived(result);
+    }
+
+    interface OnJokeEventListener {
+        void onGetJokeStarted();
+
+        void onJokeReceived(String result);
+    }
+}
---
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -0,0 +1,36 @@
+package com.udacity.gradle.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.udacity.gradle.builditbigger.javajokes.Joke;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "jokeApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.gradle.udacity.com",
+                ownerName = "backend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    /**
+     * A simple endpoint method that returns a joke from the java library
+     */
+    @ApiMethod(name = "getJokeService")
+    public MyBean getJokeService() {
+        MyBean response = new MyBean();
+
+        Joke joke = new Joke();
+        String funnyJoke = joke.getJoke();
+        response.setData(funnyJoke);
+
+        return response;
+    }
+
+}
